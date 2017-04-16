e8cdfd0e5f73aa4e8ca06effbb0cc6e3a0563ceb
曾智聖 <chr4337@gmail.com>
2017-04-16 17:54:28 +0800

    feature: Alarm show notification

--- app/src/main/java/com/viovie/example/receiver/AlarmReceiver.java
+++ app/src/main/java/com/viovie/example/receiver/AlarmReceiver.java
@@ -1,20 +1,50 @@
 package com.viovie.example.receiver;
 
 import android.app.AlertDialog;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.view.WindowManager;
+
+import com.viovie.example.DemoStartActivity;
+import com.viovie.example.R;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setTitle("TEST");
+//        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+//        builder.setTitle("TEST");
 
-        AlertDialog dialog = builder.create();
-        dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
-        dialog.show();
+//        AlertDialog dialog = builder.create();
+//        dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+//        dialog.show();
+
+
+        String title = "TEST";
+        String message = "message";
+        Intent newIntent = new Intent(context, DemoStartActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0 /* Request code */, newIntent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
 }
